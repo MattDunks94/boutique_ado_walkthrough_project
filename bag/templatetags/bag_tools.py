cf7ef79db8843b35a3145a297f04680733ba7b01
Matt <mjd014@live.co.uk>
2022-12-28 11:12:01 +0000

    added code comments.

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,8 +1,11 @@
 from django import template
 
+# Library is part of django library.
 register = template.Library()
 
 
+# This is a custom tamplate filter.
 @register.filter(name='calc_subtotal')
+# Function that we want to target in bag.html
 def calc_subtotal(price, quantity):
     return price * quantity
