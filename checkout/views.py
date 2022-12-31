0a3deb6886e8ec79e31d6473ae11e3df12544dc4
Matt <mjd014@live.co.uk>
2022-12-31 16:20:36 +0000

    added checkout view, url file, included checkout urls to main urls, created static folder with checkout css and checkout template.

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+# Checkout view.
+def checkout(request):
+    # Getting the user's shopping bag session.
+    bag = request.session.get('bag', {})
+    if not bag:
+        # Displays error message if bag doesn't exist.
+        messages.error(request, "There's nothing in your bag at the moment")
+        # Redirects back to products page.
+        return redirect(reverse('products'))
+    
+    # Order form instance.
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+    return render(request, template, context)
