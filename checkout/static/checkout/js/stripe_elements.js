f64e15adcd267f4cbcc5c61ac18eb57b3d426620
Matt <mjd014@live.co.uk>
2023-01-01 19:18:23 +0000

    added stripe elements.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -2,3 +2,27 @@
 // Collecting public key and client secret, slicing one character at either end.
 var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
 var client_secret = $('#id_client_secret').text().slice(1, -1);
+// Setting up Stripe, pass it the public key.
+var stripe = Stripe(stripe_public_key);
+// Creating an instance of Stripe elements.
+var elements = stripe.elements();
+// Creating card element.
+var card = elements.create('card', {style: style});
+// Styling the card element.(see stripe docs.)
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+};
+// Mounting the card element to the div created in checkout.html, via id.
+card.mount('#card-element');
