cb0dc2b536eccf55681fab851fec59686b4f08d4
Matt <mjd014@live.co.uk>
2023-01-02 15:01:11 +0000

    installed stripe, added stripe env variables to checkout/views, modified the checkout view.

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,10 @@ from bag.contexts import bag_contents
 
 # Checkout view.
 def checkout(request):
+    # Assigning stripe variables to new variables.
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     # Getting the user's shopping bag session.
     bag = request.session.get('bag', {})
     if not bag:
@@ -19,16 +23,33 @@ def checkout(request):
         # Redirects back to products page.
         return redirect(reverse('products'))
 
+    # Assigning bag_contents to variable.
     current_bag = bag_contents(request)
+    # Getting the bag grand total.
     total = current_bag['grand_total']
+    # Sets the total to 2 decimal places.
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        # Currency set in settings.
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     # Order form instance.
     order_form = OrderForm()
+
+    # Warning message if stripe_public_key doesn't exist.
+    if not stripe_public_key:
+        messages.warning(
+            request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?'
+            )
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MLVZwFjejZeJgUo3dWSgHcDyWfFbwRJ3DNh3b3yFAfKa3QgeYAxQgUIkUB8ipqIxW8sjr5JLFJA9kJR34BDia1400yCf0Lk0I',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
     return render(request, template, context)
