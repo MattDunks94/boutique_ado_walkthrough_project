7d66b876c2d7ea4c95efa7236cd5e0bf21619230
Matt <mjd014@live.co.uk>
2023-01-08 20:20:32 +0000

    added profile app

--- checkout/models.py
+++ checkout/models.py
@@ -8,12 +8,17 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Product
-
+from profiles.models import UserProfile
 
 # Order model.
 # References order no., customer details and grand total of payment.
 class Order(models.Model):
+
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        # SET_NULL allows users to keep order history even when deleting profile.
+        UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='Orders'
+    )
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
