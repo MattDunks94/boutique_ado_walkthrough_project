3313a58a7fd9c432e90a41c1b2815f2314ee9437
Matt <mjd014@live.co.uk>
2023-01-17 20:06:39 +0000

    added code comments

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,6 +182,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Cache control
+    # Cache's static files until the exipiration date.
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94600000',
